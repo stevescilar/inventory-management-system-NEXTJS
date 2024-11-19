d8dd7e7145a25785818db7851ca6ec49ccb38118
stevescilar <stevemuambi@gmail.com>
2024-11-19 17:52:06 +0300

    "Updated Sidebar component with new layout and added import statement in DashboardWrapper"

--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -1,9 +1,31 @@
+"use client";
+import { Menu } from 'lucide-react'
 import React from 'react'
 
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div>
+        {/* Top Logo */}
+        <div className="flex gap-3 justify-between md:justify-normal items-center pt-8">
+            <div>Logo</div>
+            <h1 className='font-extrabold text-2xl'> STOCKMAN</h1>
+        
+            <button className='md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100' onClick={() => {}}>
+                <Menu className='w-4 h-4' />
+            </button>
+        </div>
+
+        {/* Links */}
+        <div className='flex-grow mt-5'>
+            {/* links */}
+        </div>
+        {/* footer */}
+        <div>
+        <p className='text-center text-xs text-gray-500'>&copy; 2024 STOCKMAN</p>
+        </div>
+
+    </div>
   )
 }
 
